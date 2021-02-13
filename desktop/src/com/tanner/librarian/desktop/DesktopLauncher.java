b87c0b55418e9542749f727685e9d521132f9b70
Tanner <tannersbrown@gmail.com>
2021-02-13 11:33:49 -0800

    init

---
+++ desktop/src/com/tanner/librarian/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.tanner.librarian.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.tanner.librarian.Librarian;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Librarian(), config);
+	}
+}
